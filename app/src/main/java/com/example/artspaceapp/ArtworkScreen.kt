1076a274dbc42647a6da09c9d4e883356d9cde2a
CazWong <102901545+CazWong@users.noreply.github.com>
2025-05-06 18:06:00 +1000

    feat: Add ArtworkScreenLayout composable

---
+++ app/src/main/java/com/example/artspaceapp/ArtworkScreen.kt
@@ -0,0 +1,60 @@
+package com.example.artspaceapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.artspaceapp.components.ArtworkInfo
+import com.example.artspaceapp.components.ArtworkItem
+import com.example.artspaceapp.components.CarouselButtons
+
+@Composable
+fun ArtworkScreenLayout(
+    modifier: Modifier = Modifier,
+    artworkList: List<Artwork>,
+) {
+
+    var currentArtworkIndex by remember { mutableIntStateOf(0) }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 32.dp, bottom = 16.dp, start = 16.dp, end = 16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        ArtworkItem(
+            image = painterResource(
+                id = artworkList[currentArtworkIndex].artwork
+            ),
+        )
+        Spacer(modifier = Modifier.height(48.dp))
+        ArtworkInfo(
+
+            title = artworkList[currentArtworkIndex].title,
+            location = artworkList[currentArtworkIndex].location,
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        CarouselButtons(
+            currentIndex = currentArtworkIndex,
+            artworkList = artworkList,
+            onIndexChanged = { newIndex -> currentArtworkIndex = newIndex }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ArtworkScreenPreview() {
+    ArtworkScreenLayout(artworkList = artworkList)
+}
